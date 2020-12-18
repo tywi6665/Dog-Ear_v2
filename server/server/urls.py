424a7bb843bcdace5c0a6eb31efa3d36e5ef5a6c
Tyler <tyletcole@gmail.com>
2020-12-17 20:00:59 -0700

    added basic route

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
+app_name = 'dog-ear-server'
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/hello/', views.hello, name='hello'),
 ]
