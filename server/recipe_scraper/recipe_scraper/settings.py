101808e8bf0e83a0640ab2937616e4c2d6872271
Tyler <tyletcole@gmail.com>
2020-12-26 15:30:07 -0700

    Setting up spider architecture

--- server/recipe_scraper/recipe_scraper/settings.py
+++ server/recipe_scraper/recipe_scraper/settings.py
@@ -1,3 +1,16 @@
+import os
+import sys
+
+# Django integration
+sys.path.append(os.path.dirname(os.path.abspath('.')))
+# Do not forget the change iCrawler part based on your project name
+os.environ['DJANGO_SETTINGS_MODULE'] = 'server.settings'
+
+# This is required only if Django Version > 1.8
+import django
+django.setup()
+
+
 # Scrapy settings for recipe_scraper project
 #
 # For simplicity, this file contains only settings considered important or
--- server/recipe_scraper/recipe_scraper/spiders/recipe_crawler.py
+++ server/recipe_scraper/recipe_scraper/spiders/recipe_crawler.py
@@ -5,16 +5,26 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class RecipeCrawlerSpider(CrawlSpider):
     name = 'recipe_crawler'
-    allowed_domains = ['https://google.com']
-    start_urls = ['http://https://google.com/']
 
-    rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    )
+    # Dynamic methods that allows Django to pass values to crawler
+    def __init__(self, *args, **kwargs):
+        # going to pass these args from django view 
+        # To make everything dynamic, we need to override them inside __init__ method
+        self.url = kwargs.get('url')
+        self.domain = kwargs.get('domain')
+        self.start_urls = [self.url]
+        self.allowed_domains = [self.domain]
+
+        RecipeCrawlerSpider.rules = [
+            Rule(LinkExtractor(unique=True), callback='parse_item')
+        ]
+        super(RecipeCrawlerSpider, self).__init__(*args, **kwargs)
+
 
     def parse_item(self, response):
-        item = {}
+        recipe = {}
         #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
         #item['name'] = response.xpath('//div[@id="name"]').get()
         #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        recipe['url'] = response.url
+        return recipe
