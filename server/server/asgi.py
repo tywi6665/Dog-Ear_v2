a4e75aa3afd18a485e0e15184d9cb30294c294d6
Tyler <tyletcole@gmail.com>
2020-12-28 19:32:26 -0700

    setting websockets

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -13,4 +13,14 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
-application = get_asgi_application()
+django_application = get_asgi_application()
+
+async def application(scope, receive, send):
+    if scope['type'] == 'http':
+        # let django handle the HTTP requests
+        await django_application(scope, receive, send)
+    elif scope['type'] == 'websocket':
+        # let Websocket connection handle the request
+        pass
+    else:
+        raise NotImplementedError(f"Unknown scope type {scope['type']}")
