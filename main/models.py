700330863a301ac4e727dd5e0c836dc1cde1d96e
Tyler <tyletcole@gmail.com>
2021-02-01 19:44:36 -0700

    updated DB requirements

--- main/models.py
+++ main/models.py
@@ -5,12 +5,12 @@ from django.utils import timezone
 # Create your models here.
 class RecipeItem(models.Model):
     unique_id = models.CharField(max_length=100, null=True)
-    url = models.CharField(max_length=200, blank=False)
-    title = models.CharField(max_length=200, blank=False)
+    url = models.CharField(max_length=300, blank=False)
+    title = models.CharField(max_length=300, blank=False)
     author = models.CharField(max_length=200, blank=True)
     description = models.TextField(max_length=None, blank=True)
     has_made = models.BooleanField(default=False)
-    img_src = models.CharField(max_length=200, blank=True)
+    img_src = models.CharField(max_length=300, blank=True)
     notes = models.JSONField(default=list)
     rating = models.IntegerField(default=0)
     tags = models.JSONField(default=list)
