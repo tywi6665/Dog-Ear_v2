3e93a2d2b3044e5e2b8bde73f9896f45a743b73f
Tyler <tyletcole@gmail.com>
2021-02-26 07:11:14 -0700

    commit

--- main/models.py
+++ main/models.py
@@ -21,12 +21,12 @@ class RecipeItem(models.Model):
 
 class CrawledRecipeItem(models.Model):
     unique_id = models.CharField(max_length=100, null=True)
-    url = models.CharField(max_length=200, blank=False)
-    title = models.CharField(max_length=200, blank=False)
+    url = models.CharField(max_length=300, blank=False)
+    title = models.CharField(max_length=300, blank=False)
     author = models.CharField(max_length=200, blank=True)
     description = models.TextField(max_length=None, blank=True)
     has_made = models.BooleanField(default=False)
-    img_src = models.CharField(max_length=200, blank=True)
+    img_src = models.CharField(max_length=300, blank=True)
     notes = models.JSONField(default=list)
     rating = models.IntegerField(default=0)
     tags = models.JSONField(default=list)
